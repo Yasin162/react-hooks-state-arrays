7d93886791e1ea443886605b55c8f08b915e2d6a
Yasin <yasinabdussaburr@gmail.com>
2021-09-29 09:18:24 -0400

    commit tree 42fdf09e74c66239870c08ead1074f2ae2bdbe90

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,13 +6,37 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
+    const newFoodArray = [...foods, newFood]
+    setFoods(newFoodArray)
     console.log(newFood);
   }
+
+  const handleLiClick = (id) => {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
+  }
+
+  const foodList = foods.map((food) => (
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
+      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+    </li>
+  ));
+
+  const [filterBy, setFilterBy] = useState("All");
 
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{/* list of spicy foods */}</ul>
+      <ul>{/* list of spicy foods */} {foodList}</ul>
     </div>
   );
 }
